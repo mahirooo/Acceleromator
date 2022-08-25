3fb40fd7e093880fde87e49c02558f441f69fb1c
長崎茉優 <nagasaki@nagasakinoMacBook-Pro.local>
2022-08-25 14:15:46 +0900

    all done

---
+++ Acceleromator/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Acceleromator
+//
+//  Created by 長崎茉優 on 2022/08/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBAction func unwindToViewController(segue: UIStoryboardSegue){
+        
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
